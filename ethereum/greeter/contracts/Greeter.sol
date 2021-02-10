d4e707615beff0de8069763d7c92d5bd5af70e87
Alfred Tso <alfredtso@gmail.com>
2021-02-11 00:05:42 +0800

    "Added tests for the toy example: hello world and getting addresses"

--- ethereum/greeter/contracts/Greeter.sol
+++ ethereum/greeter/contracts/Greeter.sol
@@ -1,9 +1,23 @@
 pragma solidity >= 0.4.0 < 0.7.0;
 
 contract Greeter {
+	string private _greeting = "Hello, World!";
+	address private _owner;
 
-	function greet() external pure returns(string memory) {
-		return "Hello, World!";
+	constructor() public {
+		_owner = msg.sender;
+	}
+
+	function greet() external view returns(string memory) {
+		return _greeting;
+	}
+
+	function setGreeting(string calldata myString) external {
+		_greeting = myString;
+	}
+
+	function owner() public view returns(address) {
+		return _owner;
 	}
 
 }
--- ethereum/greeter/test/greeter_test.js
+++ ethereum/greeter/test/greeter_test.js
@@ -1,20 +1,51 @@
 // Truffle can load and interact with contracts compiled with artifact.require
 const GreeterContract = artifacts.require("Greeter");
 
-contract("Greeter", () => {
+contract("Greeter", (accounts) => {
 	it("has been deployed succesfully", async () => {
 		const greeter = await GreeterContract.deployed();
 		assert(greeter, "contract was not deployed");
 	});
+
+	describe('greet()', () => {
+		it('returns Hello world', async () => {
+			const greeter = await GreeterContract.deployed();
+			const expected = "Hello, World!";
+			const actual = await greeter.greet();
+
+			assert.equal(actual, expected, "greeted with 'Hello, World!");
+		});
+	});
+
+	describe('owner()', () => {
+		it('return the address of the owner', async () => {
+			const greeter = await GreeterContract.deployed();
+			const owner = await greeter.owner();
+
+			assert(owner, "current owner");
+		});	
+
+		it("matches the address that deployed the contract", async () => {
+			const greeter = await GreeterContract.deployed();
+			const owner = await greeter.owner();
+			const expected = accounts[0];
+
+			assert.equal(owner, expected, "the addresses doesn't match");
+		});
+	});
 });
 
-describe('greet()', () => {
-	it('returns Hello world', async () => {
-		const greeter = await GreeterContract.deployed();
-		const expected = "Hello, World!";
-		const actual = await greeter.greet();
 
-		assert.equal(actual, expected, "greeted with 'Hello, World!");
+contract("Greeter: update greeting", () => {
+	describe("setGreeting(string)", () => {
+		it("sets greeting to passed in string", async () => {
+			const greeter = await GreeterContract.deployed()
+			const expected = "Hi there!";
+
+			await greeter.setGreeting(expected);
+			const actual = await greeter.greet();
+
+			assert.equal(actual, expected, "greeting was not updated");
+		});
 	});
-	
 });
